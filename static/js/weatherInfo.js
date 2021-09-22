c200ecbb26288ee9a3dac3903d6f628ab96a5c08
Monish S <67294870+Monish2001@users.noreply.github.com>
2021-09-22 20:57:54 +0530

    Weather info

---
+++ static/js/weatherInfo.js
@@ -0,0 +1,71 @@
+function getWeather() {
+    // cityList = ["Delhi", "Mumbai", "Kolkata", "Bangalore", "Chennai", "Hyderabad", "Pune", "Ahmedabad", "Surat", "Luckknow", "Jaipur", "Indore", "Bhopal", "Patna", "Madurai", "Vellore", "Nellore", "Jammu", "Belgaum", "Jhansi"]
+
+    var cityName = document.getElementById("getWeatherByCity").value;
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=f847023ac7fd05f740bd61dc050203fe', {
+            method: 'GET'
+        })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            displayData(data);
+        })
+        .catch(function(err) {
+            alert("Something went wrong!!")
+            console.log("Something went wrong!", err);
+        });
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        var val = navigator.geolocation.getCurrentPosition(getWeatherForCurrentLocation);
+    } else {
+        // x.innerHTML = "Geolocation is not supported by this browser.";
+        alert("Geolocation is not supported by this browser.")
+    }
+}
+
+function getWeatherForCurrentLocation(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=f847023ac7fd05f740bd61dc050203fe', {
+            method: 'GET'
+        })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            displayData(data);
+        })
+        .catch(function(err) {
+            alert("Something went wrong!!")
+            console.log("Something went wrong!", err);
+        });
+
+}
+
+function displayData(data) {
+    var main = document.getElementById('name');
+    var temp = document.getElementById('temp');
+    var desc = document.getElementById('desc');
+    var humidity = document.getElementById('humidity');
+    var lat = document.getElementById('lat');
+    var lon = document.getElementById('lon');
+
+    var tempValue = data['main']['temp'];
+    var nameValue = data['name'];
+    var descValue = data['weather'][0]['description'];
+    var humidityValue = data['main']['humidity']
+    var latValue = data['coord']['lat']
+    var lonValue = data['coord']['lon']
+
+    main.innerHTML = nameValue;
+    desc.innerHTML = "Description - " + descValue;
+    temp.innerHTML = "Temperature - " + tempValue;
+    humidity.innerHTML = "Humidity - " + humidityValue
+    lat.innerHTML = "Latitude - " + latValue
+    lon.innerHTML = "Longitude - " + lonValue
+}
